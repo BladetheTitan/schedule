255833728a1e93a0f7b0f0056512fa1525473419
Evan Norland <bladethetitan@gmail.com>
2018-07-23 15:50:13 +0000

    library course component grid finished

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -7,8 +7,10 @@ class LibraryCourse extends Component {
     render() {
         return (
             <div className="library-course">
-                <label className="library-course__title">Problem Solving</label>
-                { Icon('fas fa-check', 'library-course__icon') }
+                <div className="library-course__title-arrow">
+                    <label className="library-course__title">Problem Solving</label>
+                    { Icon('fas fa-check', 'library-course__icon') }
+                </div>
                 <Arrow className="library-course__arrow"/>
                 { Icon('fas fa-plus-circle', 'library-course__action') }
                 <div className="library-course__description">
