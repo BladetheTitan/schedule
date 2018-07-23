5b4c376d10a00dcc17afce5ca43017cb4f7d1ff2
Evan Norland <bladethetitan@gmail.com>
2018-07-23 15:22:34 +0000

    integrated font awesome and created icons components for check and action

---
+++ src/components/library/libraryCourse.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import Icon from '../icon';
+
+class LibraryCourse extends Component {
+    render() {
+        return (
+            <div className="library-course">
+                <label className="library-course__title">Problem Solving</label>
+                { Icon('fas fa-check', 'library-course__icon') }
+                {/* arrow component */}
+                {/* action button component */}
+                <div className="library-course__description">
+                    <label>Course Description</label>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu tellus accumsan, condimentum tellus ut, faucibus ligula. Praesent viverra pharetra justo, vitae commodo nisl laoreet at.</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default LibraryCourse;
